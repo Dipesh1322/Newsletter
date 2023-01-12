17cf068b0a2b463d4184e708c10cb92c883beff1
Dipesh Chaudhary <108289234+Dipesh1322@users.noreply.github.com>
2023-01-12 19:33:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+
+app.post("/",function(req,res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const data = {
+        members : [
+            {
+                email_address : email,
+                status : "subscribed",
+                merge_fields : {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+   
+    const url = "https://us21.api.mailchimp.com/3.0/lists/c7416d5638";
+
+    const options = {
+        method : "POST",
+        auth : "dipesh13:9a694d29bfe10dc0ff8c4862061d765e-us21"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if(response.statusCode===200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+    
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server is running on Port 3000.");
+})
+
+
+
+
+// API Key
+// 9a694d29bfe10dc0ff8c4862061d765e-us21
+
+// Audience ID
+// c7416d5638
